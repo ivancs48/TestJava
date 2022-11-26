a3118535eded1386abf591a39b16d5be06ca39b0
Ivan Calderon <ivancs48@gmail.com>
2022-11-26 09:36:16 -0500

    Commit inicial del proyecto

---
+++ src/test/java/com/calderon/TestJava/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calderon.TestJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
